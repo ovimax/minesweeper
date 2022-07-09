92d57d522eb9a071bfd5ffc7005cd38cd29bd35f
ONsistems <osilvium@gmail.com>
2022-07-09 19:05:39 +0200

    0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 * Board desing
 * Beginner Level (9x9, 10 Bombs)
 * Bomb arrangement
+
+### 0.0.2 (2022-07-09)
+* Numbers arrangement
+* Bomb alert message
--- js/main.js
+++ js/main.js
@@ -5,22 +5,82 @@ let bombRand = Math.floor(Math.random() * 100) + 1;
 let cell = '<div class="cell"></div>';
 let bombCount = 0;
 let bombMax = 10;
+let boardMap = [];
 
 boardGame.style.width = boardSize+'50px';
 boardGame.style.height = boardSize+'50px';
 
 for (var i = 0; i < boardSize; i++) {
+	boardMap[i]=[];
 	for (var j = 0; j < boardSize; j++) {
-		bombRand = Math.floor(Math.random() * 100) + 1 + i + j;
-		console.log(i,j)
-		cell = '<div class="cell">'+ i+j +'</div>';
+		bombRand = Math.floor(Math.random() * 100) + 1;
+		cell = '<div class="cell" id="cell-'+i+j+'"></div>';
+		boardMap[i][j] = 0;
 		if(bombRand%5==0 && bombCount !=bombMax)
 		{
-			cell = '<div class="cell"><div class="bomb">'+ i+j +'</div></div>';
+			cell = '<div class="cell" id="cell-'+i+j+'"><div class="bomb" id="bomb"></div></div>';
 			bombCount++;
+			boardMap[i][j] = 1;
 		} 
 		
 		boardGame.innerHTML += cell;
 	}
 }
 
+for (var i = 0; i < boardSize; i++) {
+	for (var j = 0; j < boardSize; j++) {
+		if(boardMap[i][j]!=1) {
+			let number = numberCell(i,j);
+			let cellBlock = document.getElementById('cell-'+i+j);
+			cellBlock.innerHTML = `<span class="number ${number==0?"blank":"number-"+number}">${number==0?"":number}</span>`
+		}
+	}
+}
+
+function numberCell(x,y)
+{
+	let bombNumber = 0;
+	if (typeof boardMap[x-1]!=='undefined' && boardMap[x-1][y]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x+1]!=='undefined' && boardMap[x+1][y]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x][y-1]!=='undefined' && boardMap[x][y-1]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x][y+1]!=='undefined' && boardMap[x][y+1]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x-1]!=='undefined' && boardMap[x-1][y-1]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x-1]!=='undefined' && boardMap[x-1][y+1]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x+1]!=='undefined' && boardMap[x+1][y-1]==1)
+	{
+		bombNumber++;
+	}
+	if (typeof boardMap[x+1]!=='undefined' && boardMap[x+1][y+1]==1)
+	{
+		bombNumber++;
+	}
+
+	return bombNumber
+
+}
+
+let bombCells = document.getElementsByClassName('bomb');
+Array.from(bombCells).forEach(function(element) {
+	element.addEventListener('click', function(argument) {
+		alert("BOOOOM");
+		location.reload();
+	});
+});
